47118c482866f848cd6254a47a18035f224b2f71
Clairton Rodrigo Heinzen <clairton.heinzen@cresolsicoper.com.br>
2014-07-31 07:57:15 -0300

    Rewrite ratio migrate

--- app/models/ratio.rb
+++ app/models/ratio.rb
@@ -1,2 +1,6 @@
 class Ratio < ActiveRecord::Base
+  belongs_to :origin, class_name: 'Unit'
+  belongs_to :destiny, class_name: 'Unit'
+
+  validates :destiny, :origin, :formula, :enable, presence: true
 end
--- db/migrate/20140730231737_create_ratios.rb
+++ db/migrate/20140730231737_create_ratios.rb
@@ -1,9 +1,12 @@
 class CreateRatios < ActiveRecord::Migration
   def change
     create_table :ratios do |t|
-      t.integer :origin
-      t.integer :destiny
-      t.formula :value
+      t.integer :origin_id, null: false, index: true
+      t.integer :destiny_id, null: false, index: true
+      t.string :formula, null: false, index: true
+      t.boolean :enable, null: false, default: true, index: true
+
+      t.timestamps
     end
   end
 end
